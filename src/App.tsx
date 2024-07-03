a45fdefcac43192e3d030dc263ab4b46cb1c99cb
mxrsel <marsel14082006@gmail.com>
2024-07-04 02:16:05 +0600

    created home container and added home content to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-// import {Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Home from './containers/Home/Home';
 const App: React.FC = () => {
- return(
-   <header>
-     <NavBar />
-   </header>
+  return (
+    <header>
+      <NavBar/>
 
-
+      <div className="body">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </header>
   )
 };
 
---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="Home ">
+      <h1>Home page</h1>
+      <p>This is a main page of this site</p>
+    </div>
+  );
+};
+
+export default Home;
