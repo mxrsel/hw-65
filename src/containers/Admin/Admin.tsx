59841bc89e0c71be75e1afb8248050f6bc3b6cbb
mxrsel <marsel14082006@gmail.com>
2024-07-04 15:37:56 +0600

    finished visuals components on page admin

--- src/containers/Admin/Admin.tsx
+++ src/containers/Admin/Admin.tsx
@@ -2,34 +2,46 @@ import React from 'react';
 import {PAGES} from '../../constants';
 
 const Admin: React.FC= () => {
+
+
   return (
-    <div className="adminPage">
-      <h1>Edit pages</h1>
-      <form>
-      <select
-        name="page"
-        id="page"
-        required
-        className="select-page">
-        <option value="">Select Page</option>
-        {PAGES.map((page) => (
-          <option key={page.id} value={page.id}></option>
-        ))}
-      </select>
-      <label>
-        Title
-      <input
-        type="text"
-        name="title"/>
-      </label>
+    <div className="adminPage row mt-2">
+      <div className="col ">
+        <h1>Edit pages</h1>
+        <form>
+          <select
+            name="page"
+            id="page"
+            required
+            className="select-page">
+            <option value="">Select Page</option>
+            {PAGES.map((page) => (
+              <option key={page.id} value={page.id}>{page.title}</option>
+            ))}
+          </select>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              name="title"
+              id="title"
+              required
+              className="form-control"/>
+          </label>
 
-      <label>
-        Content
-        <textarea>
-
+          <label htmlFor="content">
+            Content
+            <textarea
+              name="content"
+              id="content"
+              className="form-control">
         </textarea>
-      </label>
-      </form>
+          </label>
+          <button className="btn btn-primary mt-2" type="submit">
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
